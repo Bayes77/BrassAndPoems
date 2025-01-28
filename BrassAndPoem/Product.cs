bd6e01ec2966df359afb9d203e6722aa5a907892
Bayez77 <mrbayez88@gmail.com>
2025-01-27 19:02:51 -0600

    completed project

--- BrassAndPoem/Product.cs
+++ BrassAndPoem/Product.cs
@@ -1,1 +1,33 @@
 //create your Product class here
+//create your Product class here
+using System;
+using System.Runtime.CompilerServices;
+
+namespace BrassAndPoem;
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int ProductTypeId { get; set; }
+
+
+    // This is the constructor for the Product class
+    public Product(string name, decimal price, int productTypeId)
+    {
+
+        Name = name;
+        Price = price;
+        ProductTypeId = productTypeId;
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
--- BrassAndPoem/ProductType.cs
+++ BrassAndPoem/ProductType.cs
@@ -1,1 +1,22 @@
 //create your ProductType class here
+//create your ProductType class here
+
+using System;
+
+namespace BrassAndPoem;
+
+public class ProductType
+{
+    public string Title { get; set; }
+    public int TypeId { get; set; }
+
+
+    //This is the constructor for ProductTypes
+    public ProductType(string title, int typeId)
+    {
+        Title = title;
+        TypeId = typeId;
+    }
+
+
+}
